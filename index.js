3400ef47f34128fe5f0c2611205a84dfa9720f30
github username <sharnellgrant@gmail.com>
2021-06-15 12:02:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+   .then(response => response.json())
+   .then(json => renderBooks(json))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -14,4 +17,5 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  
 });
